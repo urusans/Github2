eb994928c8e691636963249886c916f0b4df3864
URUSAN <urusans@gmail.com>
2022-07-04 21:49:21 +0300

    Öğrenci Class ı ikinci işlemler yepıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,6 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("Merhaba Dünya");
+        System.out.println("Merhaba Dünya 2");
+        System.out.println("Merhaba Dünya 3");
     }
 }
