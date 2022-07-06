9f8e0e12892fa7f525922c50945f4e29ac10abb5
ahmet <ahmetenesnamli@gmail.com>
2022-07-06 21:12:36 +0300

    yeni merhaba

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,5 +2,7 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("Merhaba Dünya 2");
         System.out.println("Merhaba Dünya 3");
+
+        System.out.println("merhaba ss");
     }
 }
